d49059165767864150d247c5b998443a95b39344
Berdonosov Maksim <berdonosov_m@mail.ru>
2023-10-16 00:48:22 +0300

    Need halp in 3 tusk.

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+file = open('zen.txt', 'r')
+list_zen = []
+for i_line in file:
+    list_zen.append(i_line)
+list_zen[-1] += '\n'
+for i_line in list_zen[::-1]:
+    print(i_line, end='')
+file.close()
