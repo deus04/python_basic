f745385802c2c3f9ca24e4cfe5a99a4e31b8d4f4
Berdonosov Maksim <berdonosov_m@mail.ru>
2023-10-16 00:48:22 +0300

    Need help with tusk 3

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+file = open('numbers.txt', 'r')
+summ = 0
+for i_line in file:
+    for i_sym in i_line:
+        if (i_sym != ' ') and (i_sym != '\n'):
+            summ += int(i_sym)
+file.close()
+
+file = open('answer.txt', 'w')
+file.write(str(summ))
--- Module22/03_files_and_folders/main.py
+++ Module22/03_files_and_folders/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+import os
+
+def count_and_getsize(any_path, count_dirs, count_files, total_size):
+    for i_elem in os.listdir(any_path):
+        i_elem = os.path.join(any_path, i_elem) # FIXME мне не нравится это переприсваивание. Это верно?
+        print(i_elem)
+        if os.path.isdir(i_elem):
+            count_dirs += 1
+            count_and_getsize(i_elem, count_dirs, count_files, total_size)
+        elif os.path.isfile(i_elem):
+            count_files += 1
+            total_size += os.path.getsize(i_elem)
+
+    return count_dirs, count_files, total_size
+
+
+#any_path = os.path.abspath(os.path.join('..','..')) # TODO с Этим путем не работает
+any_path = os.path.abspath('..')                    # TODO а с этим работает. Почему?
+print(any_path)
+count_dirs = 0
+count_files = 0
+total_size = 0
+count_dirs, count_files, total_size = count_and_getsize(any_path, count_dirs, count_files, total_size)
+print('Размер каталога (в Кб):', total_size/1024)
+print('Количество подкаталогов:', count_dirs)
+print('Количество файлов:', count_files)
+
