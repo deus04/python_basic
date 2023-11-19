05913ddcfb7586556909513439f75a65b34d813b
Berdonosov Maksim <berdonosov_m@mail.ru>
2023-11-20 00:01:35 +0300

    Need help with task 1 and 2

--- Module24/01_fight/main.py
+++ Module24/01_fight/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+import random
+
+
+class warior():
+    helth_points = 100
+    attack_damage = 20
+
+    def __init__(self, name):   #TODO не до конца понимаю для чего этот блок?
+        self.name = name        # Пока предполага, что аргументы которые мы объявляем в __init__
+                                # позже можем передавать напрямую в скобка, в заданном порядке
+
+    def punch(self, attacked):
+        attacked.helth_points -= self.attack_damage
+        print('{} атакует!\n У {} осталось {} здоровья'.format(
+            self.name, attacked.name, attacked.helth_points)
+        )
+
+
+unit_1 = warior('Scorpion')
+unit_2 = warior('Sub_Zero')
+
+while True:
+    if unit_1.helth_points > 0 and unit_2.helth_points > 0:
+        change = random.choice([unit_1.name, unit_2.name])
+        if change == unit_1.name:
+            unit_1.punch(unit_2)
+        else:
+            unit_2.punch(unit_1)
+        print('\n------------------\n')
+    else:
+        print('\nПобедил {} !!!!'.format(change))
+        break
--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,1 +1,52 @@
-# TODO здесь писать код
+import random
+from operator import attrgetter
+
+class Student():
+    full_name = 'Иван Иванов'
+    group_number = 1
+    performance = []
+    gpa = 1      #TODO не совсем пониаю, правильно ли я задаю эти
+                 # параметры, учитывая то, что позже я все переписываю
+
+    def info(self):
+        print('Ф.И.:{}\n'
+              'Номер группы:{}\n'
+              'Успеваемость:{}\n'
+              'Средний балл:{}\n'.format(
+            self.full_name, self.group_number, self.performance, self.gpa
+        ))
+
+    def create_names(self):
+        self.full_name = '{} {}'.format(
+            random.choice(['Иван', 'Алексей', 'Виктор', 'Георгий', 'Сергей']),
+            random.choice(['Иванов', 'Петров', 'Котов', 'Козлов', 'Амбучбекович'])
+        )
+
+    def create_group_number(self):
+        self.group_number = random.randint(1, 3)
+
+    def create_performance(self):
+        self.performance = [random.randint(1, 5)
+                            for i_item in range(5)]
+
+    def determine_GPA(self):
+        gpa = 0
+        for i_item in self.performance:
+            gpa += i_item
+        self.gpa = round(gpa / len(self.performance), 2)
+
+
+students_list = [Student()
+                 for i_student in range(10)]
+
+for i_student in students_list:
+    i_student.create_names()
+    i_student.create_group_number()
+    i_student.create_performance()
+    i_student.determine_GPA()
+
+sorted_list = sorted(students_list, key=attrgetter('gpa'))
+
+for i_student in sorted_list:
+    i_student.info()
+
