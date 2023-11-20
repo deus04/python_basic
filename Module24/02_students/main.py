c2d8d7abf69f6def7b86f54decd5f295c57ae341
alex <name@email.com>
2023-11-20 22:30:15 +0300

    урок 24, комментарии по 1 и 2

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,12 +1,15 @@
 import random
 from operator import attrgetter
 
-class Student():
-    full_name = 'Иван Иванов'
+class Student():  # TODO Определение класса должно отделяться от остального кода двумя пустыми строками
+    full_name = 'Иван Иванов'  # TODO Тогда класс надо назвать СтудентИванИванов :)
     group_number = 1
     performance = []
-    gpa = 1      #TODO не совсем пониаю, правильно ли я задаю эти
+    gpa = 1      # не совсем пониаю, правильно ли я задаю эти
                  # параметры, учитывая то, что позже я все переписываю
+    # TODO Конечно же нет, передавайте эти данные через параметры метода __init__, точно также как вы это делали в
+    #  предыдущем решении. Причем вместо gpa всегда используйте метод его расчета, тогда если оценок прибавится, то
+    #  средний бал будет всегда актуальным
 
     def info(self):
         print('Ф.И.:{}\n'
@@ -16,22 +19,23 @@ class Student():
             self.full_name, self.group_number, self.performance, self.gpa
         ))
 
-    def create_names(self):
+    def create_names(self):  # TODO не нужен такой метод, задавайте имена в основном коде программы, и передавайте
+                             #  создаваемому объекту класса
         self.full_name = '{} {}'.format(
             random.choice(['Иван', 'Алексей', 'Виктор', 'Георгий', 'Сергей']),
             random.choice(['Иванов', 'Петров', 'Котов', 'Козлов', 'Амбучбекович'])
         )
 
-    def create_group_number(self):
+    def create_group_number(self):  # TODO Аналогично предыдущему
         self.group_number = random.randint(1, 3)
 
-    def create_performance(self):
+    def create_performance(self):  # TODO Аналогично предыдущему
         self.performance = [random.randint(1, 5)
                             for i_item in range(5)]
 
-    def determine_GPA(self):
+    def determine_GPA(self):  # TODO имена методов пишутся как имена функций - только строчными буквами
         gpa = 0
-        for i_item in self.performance:
+        for i_item in self.performance:  # TODO для суммирования используйте функцию sum
             gpa += i_item
         self.gpa = round(gpa / len(self.performance), 2)
 
