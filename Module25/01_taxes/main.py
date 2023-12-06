2120cedba6046567236b7c864cc4cc2b10de17c9
Berdonosov Maksim <berdonosov_m@mail.ru>
2023-12-07 01:05:42 +0300

    Need help with task 1 and 2

--- Module25/01_taxes/main.py
+++ Module25/01_taxes/main.py
@@ -1,1 +1,52 @@
-# TODO здесь писать код
+class Property:
+    tax = 1
+    # TODO правильно ли будет определить переменную tax = 1, при условии что мы ее меняем в потомках?
+    #  как будто было бы правильно ее прописать в родителе
+
+    def __init__(self, worth):
+        self.worth = worth
+    pass
+
+    def tax_calculation(self):
+        total_price = self.worth * self.tax
+        return total_price
+
+
+class Apartment(Property):
+    tax = 1/1000
+    pass
+
+
+class Car(Property):
+    tax = 1 / 200
+    pass
+
+
+class CountryHouse(Property):
+    tax = 1 / 500
+    pass
+
+
+money = int(input('Сколько денег у вас есть?\n'))
+
+apartment = Apartment(int(input('Сколько стоит квартира?\n')))
+
+car = Car(int(input('Сколько стоит машина?\n')))
+
+country_house = CountryHouse(int(input('Сколько стоит дача?\n')))
+
+tax_apartment = apartment.tax_calculation()
+print('Налог на квартиру:', tax_apartment)
+tax_car = car.tax_calculation()
+print('Налог на машину:', tax_car)
+tax_country_house = country_house.tax_calculation()
+print('Налог на дачу:', tax_country_house)
+
+
+total_tax = tax_car + tax_apartment + tax_country_house
+remainder = money - total_tax
+print('\nНужно заплатить {}'.format(total_tax))
+if remainder > 0:
+    print('Все хорошо, у Вас останется еще {} '.format(remainder))
+else:
+    print('К сожалению у Вас недостаточно средств')
--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,78 @@
-# TODO здесь писать код
+import random
+
+
+class Monk:
+    def __init__(self, karma):
+        self.karma = karma
+
+
+    def one_day(self):
+        daily_karma = random.randint(1, 7)
+        return daily_karma
+
+
+class KillError(BaseException):
+    print('Не убей')
+
+
+class DrunkError(BaseException):
+    print('Не пей')
+
+
+class CarCrashError(BaseException):
+    print('Ехай давай')
+
+
+class GluttonyError(BaseException):
+    print('Не хавай')
+
+
+class DepressionError(BaseException):
+    print('Не грусти')
+
+
+monk = Monk(0)
+with open('karma.log', 'w') as file:
+    try:
+        monk.karma += monk.one_day()
+        for unluck in range(random.randint(1, 10)):
+            if unluck == 1:
+                error_name = random.choice(['KillError', 'DrunkError', 'CarCrashError',
+                                             'GluttonyError', 'DepressionError'])
+                raise error_name #TODO почемуто вызывает все ошибки сразу, а компилятор ругается, \
+                                 # raise error_name
+                                 # TypeError: exceptions must derive from BaseException
+                                 # Ошибки должны происходить от BaseException
+                                 # а как это правильно в классе прописать?
+    except KillError:
+        pass
+    except DrunkError:
+        pass
+    except CarCrashError:
+        pass
+    except GluttonyError:
+        pass
+    except DepressionError:
+        pass
+    else:
+        print("Нет ошибок")
+
+
+
+'''
+По итогу у вас может быть примерно такая структура программы:
+
+открываем файл
+
+цикл по набору кармы
+
+   try
+
+      карма += one_day()
+
+   except(ы) с указанием классов исключений, которые нужно поймать
+
+      добавляем запись в файл
+
+закрываем файл
+'''
