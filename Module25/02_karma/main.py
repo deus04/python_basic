db61f4fba383150b5dd0b0996859d44efee8b7e2
alex <name@email.com>
2023-12-07 10:20:09 +0300

    урок 25, есть недочеты

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -6,13 +6,19 @@ class Monk:
         self.karma = karma
 
 
-    def one_day(self):
+    def one_day(self):  # TODO Определение метода должно отделяться от остального кода класса одной пустой строкой
         daily_karma = random.randint(1, 7)
+        # TODO выбрасывать исключения надо тоже тут (с заданной в условии вероятностью), а ловить и обрабатывать их
+        #  надо в основном коде программы
         return daily_karma
 
 
 class KillError(BaseException):
-    print('Не убей')
+    # TODO базовый класс всех простых исключений это Exception, а BaseException это "внутренности"
+    #  реализации и обычно не используется
+    print('Не убей')  # TODO нет никакого смысла выводить это в консоль при чтении кода интерпретатором (а именно это
+                      #  происходит непосредственно перед запуском кода на исполнение).
+                      #  Тут как раз уместно использовать pass
 
 
 class DrunkError(BaseException):
@@ -35,16 +41,21 @@ monk = Monk(0)
 with open('karma.log', 'w') as file:
     try:
         monk.karma += monk.one_day()
-        for unluck in range(random.randint(1, 10)):
+        for unluck in range(random.randint(1, 10)):  # TODO весь код выбрасывания исключений перенесите в one_day
             if unluck == 1:
                 error_name = random.choice(['KillError', 'DrunkError', 'CarCrashError',
                                              'GluttonyError', 'DepressionError'])
-                raise error_name #TODO почемуто вызывает все ошибки сразу, а компилятор ругается, \
+                raise error_name # почемуто вызывает все ошибки сразу, а компилятор ругается, \
                                  # raise error_name
                                  # TypeError: exceptions must derive from BaseException
                                  # Ошибки должны происходить от BaseException
                                  # а как это правильно в классе прописать?
+            # TODO выбрасывать можно только классы исключений, а не строки. В списке выше вместо строк имён
+            #  классов используйте объекты исключений с поясняющими сообщениями. Ловить исключения и логгировать их надо
+            #  в основном коде программы (вспомните задачу 3 из модуля 23) - сделайте это одной веткой except
+            #  Доп. материал: https://pythonstart.ru/list/polzovatelskie-isklyucheniya-python?ysclid=lplh9zxilt629346438
     except KillError:
+        # TODO тут надо логгировать произошедшие исключения
         pass
     except DrunkError:
         pass
