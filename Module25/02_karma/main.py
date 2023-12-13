75fa97ba581954ae896b6f3c59ff54fb61572d80
Berdonosov Maksim <berdonosov_m@mail.ru>
2023-12-13 23:59:18 +0300

    Corrected tasks. Extra comments removed.

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -8,34 +8,30 @@ class Monk:
     def one_day(self):
         daily_karma = random.randint(1, 7)
         if random.randint(1, 10) == 1:
-            error_name = random.choice([KillError, DrunkError, CarCrashError, GluttonyError, DepressionError])
+            error_name = random.choice([KillError('Не убей'), DrunkError('Не пей'), CarCrashError('Ехай давай'),
+                                        GluttonyError('Не хавай'), DepressionError('Не грусти')])
             raise error_name
         return daily_karma
 
 
 class KillError(Exception):
-    advice = 'Не убей'
-    # TODO более общепринятым (стандартным) способом указания поясняющего сообщения, является использование механизма
-    #  уже реализованного в Exception - передавать его объекту класса исключения при его создании. В данном случае
-    #  сообщения можно указать в списке выше:
-    #  error_name = random.choice([KillError('Не пей'), ....])
-    #  тогда при логгировании поясняющего сообщения не пришлось бы указывать атрибут advice
+    pass
 
 
 class DrunkError(Exception):
-    advice = 'Не пей'
+    pass
 
 
 class CarCrashError(Exception):
-    advice = 'Ехай давай'
+    pass
 
 
 class GluttonyError(Exception):
-    advice = 'Не хавай'
+    pass
 
 
 class DepressionError(Exception):
-    advice = 'Не грусти'
+    pass
 
 
 monk = Monk(0)
@@ -44,6 +40,6 @@ with open('karma.log', 'w', encoding='utf-8') as karma_log:
         try:
             monk.karma += monk.one_day()
         except (KillError, DrunkError, CarCrashError, GluttonyError, DepressionError) as exc:
-            error_message = f'Уровень кармы - {monk.karma} - {exc.__class__.__name__} - {exc.advice}\n'
+            error_message = f'Уровень кармы - {monk.karma} - {exc.__class__.__name__} - {exc}\n'
             karma_log.write(error_message)
 
--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -2,12 +2,7 @@
 class MyDict(dict):
 
     def get(self, key):
-        result = 0
-        for i_key, i_value in self.items():  # TODO просто получите нужное с помощью метода get или по имени ключа
-                                             #  (предварительно надо проверить наличие ключа в словаре оператором in)
-            if key == i_key:
-                result = i_value
-        return result
+        return super().get(key, 0)
 
 
 my_dict = MyDict()
@@ -15,12 +10,6 @@ my_dict = MyDict()
 my_dict['one'] = 1
 my_dict['three'] = 3
 my_dict['four'] = 4
-test = my_dict.get('four')
-print(test)
+print(my_dict.get('four'))
 print(my_dict.get('five'))
 
-
-# new_dict = dict()
-# new_dict['two'] = 2
-# b = new_dict.get('3', 0)
-# print(b)
