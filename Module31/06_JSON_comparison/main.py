f3f1552aba0a0f69324b6578bf429ac6ec7c699b
alex <name@email.com>
2024-02-20 22:32:08 +0300

    урок 31, есть недочет

--- Module31/06_JSON_comparison/main.py
+++ Module31/06_JSON_comparison/main.py
@@ -18,12 +18,15 @@ def found_key(selected_elem, search_key):
     else:
         if isinstance(selected_elem, list):
             print('!!!!!!!!!!!!!!') #TODO почемуто не заходит сюда
+                                    # TODO потому что сейчас found_key всегда получает словарь, список может быть только
+                                    #  вложенным...
             for i_elem in selected_elem:
                 result = found_key(i_elem, search_key)
                 if result:
                     break
         else:
             for i_value in selected_elem.values():
+                # TODO ...вот тут могут быть списки
                 if isinstance(i_value, dict):
                     result = found_key(i_value, search_key)
                     if result:
@@ -41,7 +44,7 @@ with open('json_new.json', 'r', encoding='utf-8') as file:
 
 dict_of_difs = dict()
 
-diff_list = ["cost_per_unit", "staff", "datetime"]  #  указал ключ записи словаря вложенного в список, ваш алгоритм
+diff_list = ["services", "staff", "datetime"]  #  указал ключ записи словаря вложенного в список, ваш алгоритм
                                                     #  такие словари просто не "обходит" (не проверяет)
 #  В модуле 21 мы делали рекурсивный поиск ключа сложной вложенной структуры (задача 2), используйте её для решения
 #  данной задачи. Только, чтобы решение было общим (для любых вложенных структур) в неё ещё надо добавить обход
@@ -66,8 +69,8 @@ result = dict_of_difs
 print(result)
 
 with open('result.json', 'w', encoding='utf-8') as file:
-    json.dump(result, file, indent=4)  # TODO только title в итоговом файле чтото поехал(
-
+    json.dump(result, file, indent=4)  #  только title в итоговом файле чтото поехал(
+    # TODO укажите параметр ensure_ascii=False чтобы json мог содержать не только с латинские буквы
 
 
 #{'services': [{'id': 22222225, 'title': 'Ð¡ÑÑÐ¸Ð¶ÐºÐ°', 'cost': 1500, 'cost_per_unit': 1500, 'first_cost': 1500, 'amount': 1}], 'datetime': '2022-01-25T13:00:00+03:00'}
