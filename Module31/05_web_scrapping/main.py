8c609619489baf91808fedd785e11f5bd3346a5e
alex <name@email.com>
2024-02-20 19:38:17 +0300

    урок 31, есть недочет

--- Module31/05_web_scrapping/main.py
+++ Module31/05_web_scrapping/main.py
@@ -7,6 +7,7 @@ with open('examples.html', 'r') as f:
     text = f.read()
 # По итогу вы так же получаете код сайта в виде одной строки
 result = re.findall(r'<h3>.*</h3>', text) #TODO возможно можно както более  красиво сразу заголовки достать
+# TODO да, используйте группы (круглые скобки): re.findall(r'<h3.*?>(.*?)</h3>', text)
 h3_list = []
 for i_elem in result:
     i_elem = i_elem[4:-5]
